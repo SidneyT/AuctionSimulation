917c1d1b755d680b5d41b313a1b851cf64eac5e2
SidneyT <loktingtsang@gmail.com>
2013-07-15 22:13:13 +1200

    Summary statistic classes. Max, min, avg. etc.

---
+++ src/util/SumStat.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.util.List;
+
+public enum SumStat implements SumStatI {
+	Mean {
+		@Override
+		public double summaryValue(List<Double> values) {
+			IncrementalMean mean = new IncrementalMean();
+			for (double value : values) {
+				mean.addNext(value);
+			}
+			return mean.average();
+		}
+	},
+	Median {
+		@Override
+		public double summaryValue(List<Double> values) {
+			int middleElement = values.size() / 2;
+			double median;
+			if (values.isEmpty()) {
+				median = Double.NaN;
+			} else if (values.size() % 2 == 1) {
+				median = values.get(middleElement);
+			} else
+				median = (values.get(middleElement) + values.get(middleElement - 1)) / 2;
+			
+			return median;
+		}
+	},
+	Max {
+		@Override
+		public double summaryValue(List<Double> values) {
+			double currentMax = Double.NEGATIVE_INFINITY;
+			for (double value : values) {
+				if (value > currentMax)
+					currentMax = value;
+			}
+			return currentMax;
+		}
+	};
+}
---
+++ src/util/SumStatI.java
@@ -0,0 +1,7 @@
+package util;
+
+import java.util.List;
+
+public interface SumStatI {
+	double summaryValue(List<Double> values);
+}
