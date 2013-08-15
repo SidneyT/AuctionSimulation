cd74118e9b9f91381f9e9d30fb2e0ae12e8180ad
SidneyT <loktingtsang@gmail.com>
2013-08-15 22:34:20 +1200

    Rewrote the class to use Puppet. added some comments

--- src/agents/repFraud/MultipleRepFraud.java
+++ src/agents/repFraud/MultipleRepFraud.java
@@ -1,8 +1,5 @@
 package agents.repFraud;
 
-import java.sql.Array;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -13,39 +10,40 @@ import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 
-import javax.management.RuntimeErrorException;
-
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 import org.apache.commons.math3.util.Pair;
-
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import com.google.common.collect.Multimap;
-
-import agents.EventListener;
-import agents.SimpleUserI;
-import agents.puppets.Puppet;
-import agents.puppets.PuppetMaster;
 
 import simulator.AgentAdder;
 import simulator.AuctionHouse;
 import simulator.buffers.BufferHolder;
 import simulator.buffers.ItemSender;
-import simulator.buffers.PaymentSender;
 import simulator.buffers.ItemSender.ItemSold;
+import simulator.buffers.PaymentSender;
 import simulator.buffers.PaymentSender.Payment;
 import simulator.categories.ItemType;
 import simulator.objects.Auction;
 import simulator.objects.Bid;
-import simulator.objects.Feedback;
 import simulator.objects.Item;
-import simulator.objects.ItemCondition;
-import simulator.objects.Feedback.Val;
 import simulator.records.UserRecord;
 import util.Sample;
+import agents.EventListener;
+import agents.SimpleUserI;
+import agents.shills.Controller;
+import agents.shills.puppets.Puppet;
+import agents.shills.puppets.PuppetI;
 
-public class MultipleRepFraud extends EventListener {
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.Multimap;
+
+
+/**
+ * Fraud type where a collection of Puppets both submit auctions and bid on them.
+ * Auctions submitted by a puppet will be bid on by other puppets.
+ * Puppets will also bid in normal auctions that have a low price (<= 150)
+ */
+public class MultipleRepFraud extends EventListener implements Controller {
 
 	final protected BufferHolder bh;
 	final protected PaymentSender ps;
@@ -54,44 +52,41 @@ public class MultipleRepFraud extends EventListener {
 	private final List<ItemType> types;
 	private final int repTarget;
 	
-	private final HashBiMap<Puppet, Integer> puppets; // Map<Puppet, Index>
-	private final Map<Puppet, Long> nextBidTimes; // Map<Puppet, Time>
-	private final Map<Puppet, Long> nextAuctionTimes; // Map<Puppet, Time>
+	private final ImmutableBiMap<PuppetI, Integer> puppets; // Map<Puppet, Index>
+	private final Map<PuppetI, Long> nextBidTimes; // Map<Puppet, Time>
+	private final Map<PuppetI, Long> nextAuctionTimes; // Map<Puppet, Time>
 	
 	// for generating the times for submitting bids and auctions 
 	private final ExponentialDistribution expBid;
 	private final ExponentialDistribution expAuc;
 	
-	private final PuppetMaster master;
-	
 	private final boolean[][] interactionMatrix;
 	
 	private final Random r = new Random();
 	
-	public MultipleRepFraud(BufferHolder bh, PaymentSender ps, ItemSender is, AuctionHouse ah, UserRecord ur, List<ItemType> types, int numberOfFraudUsers, int repTarget) {
+	public MultipleRepFraud(BufferHolder bh, PaymentSender ps, ItemSender is, AuctionHouse ah, UserRecord ur, List<ItemType> itemTypes, int numberOfFraudUsers, int repTarget) {
 		super(bh);
 		this.bh = bh;
 		this.ps = ps;
 		this.is = is;
 		this.ah = ah;
-		this.types = types;
+		this.types = itemTypes;
 		
 		this.repTarget = repTarget;
 
-		master = createMaster();
-		
-		puppets = HashBiMap.create(numberOfFraudUsers);
+		HashBiMap<PuppetI, Integer> tempPuppets = HashBiMap.create(numberOfFraudUsers);
 		for (int i = 0; i < numberOfFraudUsers; i++) {
-			Puppet puppet = new Puppet(bh, ps, is, ah, master);
+			PuppetI puppet = new Puppet(bh, ps, is, ah, this, itemTypes);
 			ur.addUser(puppet);
 			ah.registerForSniping(puppet); // to get notifications when the auctions are about to end, for bidding on low priced items
-			puppets.put(puppet, puppets.size());
+			tempPuppets.put(puppet, tempPuppets.size());
 		}
+		puppets = ImmutableBiMap.copyOf(tempPuppets);
 		
 		// on average, try to get 1 rep every 3 days.
 		expBid = new ExponentialDistribution(3 * 24 * 60 / 5);
 		nextBidTimes = new HashMap<>();
-		for (Puppet puppet : puppets.keySet()) {
+		for (PuppetI puppet : puppets.keySet()) {
 			nextBidTimes.put(puppet, Math.round(expBid.sample()));
 		}
 
@@ -102,7 +97,7 @@ public class MultipleRepFraud extends EventListener {
 			expAuc = new ExponentialDistribution(Double.MAX_VALUE);
 		}
 		nextAuctionTimes = new HashMap<>(); 
-		for (Puppet puppet : puppets.keySet()) {
+		for (PuppetI puppet : puppets.keySet()) {
 			nextAuctionTimes.put(puppet, Math.round(expAuc.sample()));
 		}
 		
@@ -112,21 +107,21 @@ public class MultipleRepFraud extends EventListener {
 	
 	
 	
-	Multimap<Long, Pair<Auction, Puppet>> bidInFutureMap = ArrayListMultimap.create();
-	private void bidInFuture(long timeToBid, Auction auction, Puppet puppet) {
-		bidInFutureMap.put(timeToBid, new Pair<Auction, Puppet>(auction, puppet));
+	Multimap<Integer, Pair<Auction, PuppetI>> bidInFutureMap = ArrayListMultimap.create();
+	private void bidInFuture(int timeToBid, Auction auction, PuppetI puppet) {
+		bidInFutureMap.put(timeToBid, new Pair<Auction, PuppetI>(auction, puppet));
 	}
 	
 	@Override
-	public void newAction(Auction auction, long time) {
+	public void newAction(Auction auction, int time) {
 		super.newAction(auction, time);
 		
 		// Check if the auction is by known repFraud. 
 		// If yes, assign one of the others to bid in it. Else do nothing.
 		if (puppets.containsKey(auction.getSeller())) {
-			Puppet picked = pickUninteractedPuppet(auction.getSeller());
+			PuppetI picked = pickUninteractedPuppet(auction.getSeller());
 			bidInFuture(auction.getEndTime() - 3, auction, picked);
-			addInteraction((Puppet) auction.getSeller(), picked);
+			addInteraction((PuppetI) auction.getSeller(), picked);
 		}
 		
 	}
@@ -140,7 +135,7 @@ public class MultipleRepFraud extends EventListener {
 		
 		// check if any bids should be made now from bidInFutureMap
 		if (bidInFutureMap.containsKey(bh.getTime())) {
-			for (Pair<Auction, Puppet> pair : bidInFutureMap.get(bh.getTime())) {
+			for (Pair<Auction, PuppetI> pair : bidInFutureMap.get(bh.getTime())) {
 				Auction auction = pair.getKey();
 				if (!auction.hasNoBids())
 					throw new RuntimeException("Error. RepFraud Auction should not have any bids yet.");
@@ -154,10 +149,10 @@ public class MultipleRepFraud extends EventListener {
 	}
 	
 	private void testAndSubmit() {
-		for (Entry<Puppet, Long> puppetEntry : nextAuctionTimes.entrySet()) {
-			Puppet puppet = puppetEntry.getKey();
+		for (Entry<PuppetI, Long> puppetEntry : nextAuctionTimes.entrySet()) {
+			PuppetI puppet = puppetEntry.getKey();
 			// first test if there anyone you haven't interacted with in the group
-			if (!interactionsLeft(puppet))
+			if (!hasInteractionsLeft(puppet))
 				continue; // if already interacted with everyone, continue;
 
 			// next test if the time has arrived
@@ -172,14 +167,14 @@ public class MultipleRepFraud extends EventListener {
 		}
 	}
 	
-	private void submitAuction(Puppet puppet) {
+	private void submitAuction(PuppetI puppet) {
 		Item newItem = new Item(types.get(r.nextInt(types.size())), "item" + r.nextInt(100000));
 		Auction auction = new Auction(puppet, newItem, 2016, 100, 0, 0.0); // auction has 0 popularity so no one will bid on it.
 		bh.getAuctionMessagesToAh().put(auction);
 		
 	}
 
-	private boolean interactionsLeft(Puppet puppet) {
+	private boolean hasInteractionsLeft(PuppetI puppet) {
 		int puppetIndex = puppets.get(puppet);
 		boolean[] row = interactionMatrix[puppets.get(puppet)];
 		for (int i = 0; i < interactionMatrix.length; i++) 
@@ -188,7 +183,7 @@ public class MultipleRepFraud extends EventListener {
 		return false;
 	}
 	
-	private Puppet pickUninteractedPuppet(SimpleUserI puppet) {
+	private PuppetI pickUninteractedPuppet(SimpleUserI puppet) {
 		Sample<Integer> sampler = new Sample<>(1);
 		
 		int puppetIndex = puppets.get(puppet);
@@ -197,7 +192,7 @@ public class MultipleRepFraud extends EventListener {
 				sampler.add(i);
 		}
 		
-		Puppet chosenPuppet;
+		PuppetI chosenPuppet;
 		if (sampler.getSample().isEmpty()) {
 			chosenPuppet = null;
 			throw new RuntimeException("Execution should never reach here.");
@@ -208,58 +203,11 @@ public class MultipleRepFraud extends EventListener {
 		return chosenPuppet;
 	}
 	
-	private void addInteraction(Puppet p1, Puppet p2) {
+	private void addInteraction(PuppetI p1, PuppetI p2) {
 		int p1Index = puppets.get(p1);
 		int p2Index = puppets.get(p2);
 		interactionMatrix[p1Index][p2Index] = true;
 		interactionMatrix[p2Index][p1Index] = true;
-	}
-	
-	private PuppetMaster createMaster() {
-		return new PuppetMasterAdapter() {
-			// acting as buyer
-			@Override
-			public void puppetWinAction(Puppet puppet, Auction auction) {
-				ps.send(2, auction, auction.getCurrentPrice(), puppet, auction.getSeller());
-				awaitingItem.add(auction);
-			}
-
-			// acting as buyer
-			@Override
-			public void puppetItemReceivedAction(Puppet puppet, Set<ItemSold> items) {
-				for (ItemSold item : items) {
-					boolean awaiting = awaitingItem.remove(item.getAuction());
-					assert awaiting;
-					
-					Feedback feedback = new Feedback(Val.POS, puppet, item.getAuction());
-					System.out.println(puppet + " submitting received feedback: " + feedback);
-					bh.getFeedbackToAh().put(feedback);
-				}
-			}
-
-			@Override
-			public void puppetEndSoonAction(Puppet puppet, Auction auction) {
-				if (auction.minimumBid() <= 150) {
-					testAndBid(puppet, auction);
-				}
-			}
-
-			// acting as seller 
-			@Override
-			public void puppetGotPaidAction(Puppet puppet, Collection<Payment> paymentSet) {
-				for (Payment payment : paymentSet) {
-					boolean exists = awaitingPayment.remove(payment.getAuction());
-					assert(exists); 
-					
-					is.send(2, payment.getAuction(), payment.getAuction().getItem(), ItemCondition.GOOD, puppet, payment.getSender());
-					
-					Feedback feedback = new Feedback(Val.POS, puppet, payment.getAuction());
-					System.out.println(puppet + " submitting paid feedback: " + feedback);
-					bh.getFeedbackToAh().put(feedback);
-				}
-			}
-
-		};
 	}
 	
 	/**
@@ -267,7 +215,7 @@ public class MultipleRepFraud extends EventListener {
 	 * The criteria are: if target netRep has not been met, if nextBidTime has passed. 
 	 * @param auction
 	 */
-	private void testAndBid(Puppet puppet, Auction auction) {
+	private void testAndBid(PuppetI puppet, Auction auction) {
 		if (puppet.getReputationRecord().getNetRep() < repTarget && nextBidTimes.get(puppet) < bh.getTime()) {
 			bh.getBidMessageToAh().put(auction, new Bid(puppet, auction.minimumBid()));
 
@@ -298,6 +246,40 @@ public class MultipleRepFraud extends EventListener {
 		};
 	}
 
-	
+	@Override
+	public void winAction(SimpleUserI agent, Auction auction) {
+	}
+
+	@Override
+	public void lossAction(SimpleUserI agent, Auction auction) {
+	}
+
+	@Override
+	public boolean isFraud(Auction auction) {
+		return false;
+	}
+
+	@Override
+	public void itemReceivedAction(PuppetI agent, Set<ItemSold> itemSet) {
+	}
+
+	@Override
+	public void soldAction(SimpleUserI agent, Auction auction) {
+	}
+
+	@Override
+	public void expiredAction(SimpleUserI agent, Auction auction) {
+	}
+
+	@Override
+	public void gotPaidAction(SimpleUserI agent, Collection<Payment> paymentSet) {
+	}
+
+	@Override
+	public void endSoonAction(PuppetI agent, Auction auction) {
+		if (puppets.containsKey(auction.getSeller()) && auction.minimumBid() <= 150) {
+			testAndBid(agent, auction);
+		}
+	}
 
 }
