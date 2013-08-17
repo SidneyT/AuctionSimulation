419a0f529654a18c33d932eb5784ad67119f394b
SidneyT <loktingtsang@gmail.com>
2013-08-18 01:06:06 +1200

    Split up the Main class. Methods that start the simulation are in Main. The timer loop/thread/agent stuff is in Simulation.

--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -1,64 +1,13 @@
 package simulator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.apache.log4j.Logger;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-
-import createUserFeatures.ClusterAnalysis;
-
-import simulator.buffers.BidsToAh;
-import simulator.buffers.BufferHolder;
-import simulator.buffers.ItemSender;
-import simulator.buffers.MessagesToAh;
-import simulator.buffers.MessagesToUsers;
-import simulator.buffers.PaymentSender;
-import simulator.buffers.TimeMessage;
-import simulator.categories.CategoryRecord;
-import simulator.categories.CreateItemTypes;
-import simulator.categories.ItemType;
-import simulator.categories.CreateCategories;
 import simulator.database.DBConnection;
 import simulator.database.SaveToDatabase;
-import simulator.database.SaveObjects;
 import simulator.database.SimulationCreateTableStmts;
-import simulator.objects.Auction;
-import simulator.objects.Auction.AuctionLength;
-import simulator.objects.Feedback;
-import simulator.records.UserRecord;
-import util.CrashOnAssertionErrorRunnable;
-import agents.EventListener;
-import agents.EventListenerI;
-import agents.SimpleUser;
-import agents.SimpleUserI;
-import agents.bidders.ClusterBidder;
-import agents.bidders.ClusterEarly;
-import agents.bidders.ClusterSnipe;
 import agents.repFraud.RepFraudController;
-import agents.sellers.TMSeller;
-import agents.sellers.TimedSeller;
-import agents.shills.Hybrid;
-import agents.shills.HybridLowPrice;
-import agents.shills.HybridT;
-import agents.shills.HybridTVaryCollusion;
-import agents.shills.SimpleShillPair;
-import agents.shills.puppets.PuppetClusterBidderCombined;
-import agents.shills.strategies.LateStartTrevathanStrategy;
-import agents.shills.strategies.LowPriceStrategy;
-import agents.shills.strategies.Strategy;
-import agents.shills.strategies.TrevathanStrategy;
-import agents.shills.strategies.WaitStartStrategy;
 
 public class Main {
 
-	private static final int NUMBER_OF_THREADS = 3;
-	private final static Logger logger = Logger.getLogger(Main.class);
+	
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Start.");
@@ -74,15 +23,15 @@ public class Main {
 
 
 	public static void withFraudsIntoDiffDatabases() {
-		Strategy travethan = new TrevathanStrategy(0.95, 0.85, 0.85);
-		Strategy waitStart = new WaitStartStrategy(0.95, 0.85, 0.85);
-		Strategy lowPrice = new LowPriceStrategy();
-
-		String databaseName = "syn_normal_10k_test8";
-//		DBConnection.createDatabase(databaseName);
-//		SimulationCreateTableStmts.createSimulationTables(databaseName);
-		// run the simulation and store everything into the database
-		Main.run(SaveToDatabase.instance(databaseName));
+//		Strategy travethan = new TrevathanStrategy(0.95, 0.85, 0.85);
+//		Strategy waitStart = new WaitStartStrategy(0.95, 0.85, 0.85);
+//		Strategy lowPrice = new LowPriceStrategy();
+//
+//		String databaseName = "syn_normal_10k_test8";
+////		DBConnection.createDatabase(databaseName);
+////		SimulationCreateTableStmts.createSimulationTables(databaseName);
+//		// run the simulation and store everything into the database
+//		Main.run(SaveToDatabase.instance(databaseName));
 
 //		AgentAdder trevathan = SimpleShillPair.getAgentAdder(30, travethan); // can use 20, since each submits 10 auctions.
 //		for (int i = 0; i < 30; i++) {
@@ -136,159 +85,25 @@ public class Main {
 //			// run the simulation and store everything into the database
 //			Main.run(SaveToDatabase.instance(databaseName), hybrid);
 //		}
-//		AgentAdder repFraud = RepFraudController.getAgentAdder(1, 20, 800);
-//		for (int i = 0; i < 5; i++) {
-//			String databaseName = "syn_repFraud_100k_" + i;
-//			// construct database and tables to store simulation data
-//			DBConnection.createDatabase(databaseName);
-//			SimulationCreateTableStmts.createSimulationTables(databaseName);
-//			
-//			// run the simulation and store everything into the database
-//			Main.run(SaveToDatabase.instance(databaseName), repFraud);
-//			System.out.println("finished run " + i);
-//		}
+		AgentAdder repFraud = RepFraudController.getAgentAdder(1, 40, 800);
+		for (int i = 0; i < 5; i++) {
+			String databaseName = "syn_repFraud_100k_" + i;
+			// construct database and tables to store simulation data
+			DBConnection.createDatabase(databaseName);
+			SimulationCreateTableStmts.createSimulationTables(databaseName);
+			
+			// run the simulation and store everything into the database
+			Simulation.run(SaveToDatabase.instance(databaseName), repFraud);
+			System.out.println("finished run " + i);
+		}
 }
 	
 	/**
 	 * Generates synthetic data with only normal users.
 	 */
 	public static void normalsOnly() {
-		Main.run(SaveToDatabase.instance());
+		Simulation.run(SaveToDatabase.instance());
 //		ClusterAnalysis.clusterSimData("");
-	}
-	
-	/**
-	 * Runs the simulator
-	 */
-	public static void run(SaveObjects saveObjects, AgentAdder... userAdder) {
-		try {
-			go(saveObjects, userAdder);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-//		CalculateStats.calculateStats();
-	}
-
-	private static void go(SaveObjects saveObjects, AgentAdder... agentAdder) throws InterruptedException {
-		// create buffers
-		final TimeMessage timeMessage = new TimeMessage();
-		final MessagesToUsers messagesToUsers = new MessagesToUsers();
-		final MessagesToAh<Auction> auctionsToAh = new MessagesToAh<Auction>();
-		final MessagesToAh<Feedback> feedbackToAh = new MessagesToAh<Feedback>();
-		final BidsToAh bidsToAh = new BidsToAh();
-		final BufferHolder bh = new BufferHolder(timeMessage, messagesToUsers, auctionsToAh, feedbackToAh, bidsToAh);
-
-		// create item and payment senders
-		final PaymentSender ps = new PaymentSender();
-		final ItemSender is = new ItemSender();
-
-		// create item types
-//		final CategoryRecord cr = CreateCategories.mockCategories();
-//		ArrayList<ItemType> types = CreateItemTypes.mockItems(20, cr.getCategories());
-		final CategoryRecord cr = CreateCategories.TMCategories();
-		ArrayList<ItemType> itemTypes = CreateItemTypes.TMItems(cr.getRoot().getChildren());
-
-		saveObjects.saveCategories(cr.getCategories());
-		saveObjects.saveItemTypes(itemTypes);
-
-		final UserRecord userRecord = new UserRecord();
-		final AuctionHouse ah = new AuctionHouse(userRecord, bh, saveObjects);
-
-		// parameters of simulator
-		final int numClusterBidder = 10000;
-		final double numberOfDays = 100;
-//		final double auctionsPerBidder = 1.04; // value from TM data
-//		final double auctionsPerBidder = 1.25; // assuming 35.9% of auctions have no bids, 1.04 becomes 1.62
-		final double auctionsPerBidder = 1.4;
-//		final double failedAuctionsAdjustment = 1.033; // every 1.033 auctions, only 1 will be bid upon by at least 1 person
-		final double auctionsPerDay = auctionsPerBidder * numClusterBidder / numberOfDays; // * failedAuctionsAdjustment;
-
-//		double probIsSniper = 0.33; // from tradeMeData
-		final double probIsSniper = 0.5;
-		int numberOfSnipers = (int) (probIsSniper * numClusterBidder + 0.5);
-		for (int i = 0; i < numberOfSnipers; i++) {
-			userRecord.addUser(new ClusterSnipe(bh, ps, is, ah, itemTypes));
-		}
-		for (int i = 0; i < numClusterBidder - numberOfSnipers; i++) {
-			userRecord.addUser(new ClusterEarly(bh, ps, is, ah, itemTypes));
-		}
-//		System.out.println("average: " + ClusterBidder.debugAverage);
-
-		// add sellers
-//		int numSellers = TimedSeller.sellersRequired(auctionsPerDay);
-//		// creating normal sellers
-//		for (int i = 0; i < numSellers; i++) {
-//			userRecord.addUser(new TimedSeller(bh, ps, is, ah, types));
-//		}
-		int numSellers = TMSeller.sellersRequired(auctionsPerDay);
-		// creating normal sellers
-		for (int i = 0; i < numSellers; i++) {
-			userRecord.addUser(new TMSeller(bh, ps, is, ah, itemTypes));
-		}
-		
-		// TODO: Add fraud agents here
-		// initialise shillers; shillers are different from normal agents - their constructors add additional
-		// agents to the UserRecord
-		for (int i = 0; i < agentAdder.length; i++) {
-			agentAdder[i].add(bh, ps, is, ah, userRecord, itemTypes);
-		}
-		
-		// print out the list of users
-		logger.debug(userRecord);
-
-		ExecutorService es = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-
-		// creating the callables
-//		final Callable<Object> ahCallable = Executors.callable(new CrashOnAssertionErrorRunnable(ah));
-		final ImmutableList<Callable<Object>> callables = ImmutableList.of(
-				Executors.callable(ps),
-				Executors.callable(is)
-				);
-		final ImmutableSet.Builder<Callable<Object>> userCallablesBuilder = ImmutableSet.builder();
-		for (SimpleUserI user : userRecord.getUsers()) {
-			userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(user)));
-//			userCallablesBuilder.add(Executors.callable(user));
-		}
-		for (EventListener listener : ah.getEventListeners()) {
-			userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(listener)));
-//			userCallablesBuilder.add(Executors.callable(listeners));
-		}
-		for (Runnable runnable : ah.getRunnables()) {
-			userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(runnable)));
-//			userCallablesBuilder.add(Executors.callable(listeners));
-		}
-		
-		ImmutableSet<Callable<Object>> userCallables = userCallablesBuilder.build();
-		
-		// starting the loops - each loop is 1 time unit
-		// 24 * 60 / 5 == 1 day
-		int timeUnits = AuctionLength.ONE_DAY.length() * 100;
-//		long tenPercent = (long) (timeUnits/10);
-		for (int i = 0; i < timeUnits; i++) {
-//			if ((i + 1) % tenPercent == 0) {
-//				logger.warn("another 10% done");
-//			}
-//			System.out.println("time unit: " + i);
-			try {
-				ah.run();
-				bh.startUserTurn();
-				
-				// invokeAll() blocks until all tasks are complete
-				es.invokeAll(callables); 
-				es.invokeAll(userCallables);
-				
-				bh.startAhTurn();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// clean up
-		ah.saveUsers();
-		es.shutdown();
-		saveObjects.cleanup();
-
-		logger.debug("Simulation done.");
 	}
 
 }
---
+++ src/simulator/Simulation.java
@@ -0,0 +1,184 @@
+package simulator;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+
+import agents.EventListener;
+import agents.SimpleUserI;
+import agents.bidders.ClusterEarly;
+import agents.bidders.ClusterSnipe;
+import agents.sellers.TMSeller;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import simulator.buffers.BidsToAh;
+import simulator.buffers.BufferHolder;
+import simulator.buffers.ItemSender;
+import simulator.buffers.MessagesToAh;
+import simulator.buffers.MessagesToUsers;
+import simulator.buffers.PaymentSender;
+import simulator.buffers.TimeMessage;
+import simulator.categories.CategoryRecord;
+import simulator.categories.CreateCategories;
+import simulator.categories.CreateItemTypes;
+import simulator.categories.ItemType;
+import simulator.database.SaveObjects;
+import simulator.objects.Auction;
+import simulator.objects.Auction.AuctionLength;
+import simulator.objects.Feedback;
+import simulator.records.UserRecord;
+import util.CrashOnAssertionErrorRunnable;
+
+/**
+ * Contains the methods for running the simulation.
+ */
+public class Simulation {
+
+	final static Logger logger = Logger.getLogger(Simulation.class);
+
+	static final int NUMBER_OF_THREADS = 3;
+	
+	/**
+	 * Runs the simulator
+	 */
+	public static void run(SaveObjects saveObjects, AgentAdder... agentAdders) {
+		try {
+			Simulation.run(saveObjects, 4000, agentAdders);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+//		CalculateStats.calculateStats();
+	}
+
+	public static void run(SaveObjects saveObjects, int numberOfBidders, AgentAdder... agentAdders) throws InterruptedException {
+		Simulation.go(saveObjects, numberOfBidders, agentAdders);
+	}
+
+	static void go(SaveObjects saveObjects, int numberOfBidders, AgentAdder... agentAdders) throws InterruptedException {
+			// create buffers
+			final TimeMessage timeMessage = new TimeMessage();
+			final MessagesToUsers messagesToUsers = new MessagesToUsers();
+			final MessagesToAh<Auction> auctionsToAh = new MessagesToAh<Auction>();
+			final MessagesToAh<Feedback> feedbackToAh = new MessagesToAh<Feedback>();
+			final BidsToAh bidsToAh = new BidsToAh();
+			final BufferHolder bh = new BufferHolder(timeMessage, messagesToUsers, auctionsToAh, feedbackToAh, bidsToAh);
+	
+			// create item and payment senders
+			final PaymentSender ps = new PaymentSender();
+			final ItemSender is = new ItemSender();
+	
+			// create item types
+	//		final CategoryRecord cr = CreateCategories.mockCategories();
+	//		ArrayList<ItemType> types = CreateItemTypes.mockItems(20, cr.getCategories());
+			final CategoryRecord cr = CreateCategories.TMCategories();
+			ArrayList<ItemType> itemTypes = CreateItemTypes.TMItems(cr.getRoot().getChildren());
+	
+			saveObjects.saveCategories(cr.getCategories());
+			saveObjects.saveItemTypes(itemTypes);
+	
+			final UserRecord userRecord = new UserRecord();
+			final AuctionHouse ah = new AuctionHouse(userRecord, bh, saveObjects);
+	
+			// parameters of simulator
+	//		final int numberOfBidders = 100000;
+			final double numberOfDays = 100;
+	//		final double auctionsPerBidder = 1.04; // value from TM data
+	//		final double auctionsPerBidder = 1.25; // assuming 35.9% of auctions have no bids, 1.04 becomes 1.62
+			final double auctionsPerBidder = 1.4;
+	//		final double failedAuctionsAdjustment = 1.033; // every 1.033 auctions, only 1 will be bid upon by at least 1 person
+			final double auctionsPerDay = auctionsPerBidder * numberOfBidders / numberOfDays; // * failedAuctionsAdjustment;
+	
+	//		double probIsSniper = 0.33; // from tradeMeData
+			final double probIsSniper = 0.5;
+			int numberOfSnipers = (int) (probIsSniper * numberOfBidders + 0.5);
+			for (int i = 0; i < numberOfSnipers; i++) {
+				userRecord.addUser(new ClusterSnipe(bh, ps, is, ah, itemTypes));
+			}
+			for (int i = 0; i < numberOfBidders - numberOfSnipers; i++) {
+				userRecord.addUser(new ClusterEarly(bh, ps, is, ah, itemTypes));
+			}
+	//		System.out.println("average: " + ClusterBidder.debugAverage);
+	
+			// add sellers
+	//		int numSellers = TimedSeller.sellersRequired(auctionsPerDay);
+	//		// creating normal sellers
+	//		for (int i = 0; i < numSellers; i++) {
+	//			userRecord.addUser(new TimedSeller(bh, ps, is, ah, types));
+	//		}
+			int numSellers = TMSeller.sellersRequired(auctionsPerDay);
+			// creating normal sellers
+			for (int i = 0; i < numSellers; i++) {
+				userRecord.addUser(new TMSeller(bh, ps, is, ah, itemTypes));
+			}
+			
+			// TODO: Add fraud agents here
+			// initialise shillers; shillers are different from normal agents - their constructors add additional
+			// agents to the UserRecord
+			for (int i = 0; i < agentAdders.length; i++) {
+				agentAdders[i].add(bh, ps, is, ah, userRecord, itemTypes);
+			}
+			
+			// print out the list of users
+			logger.debug(userRecord);
+	
+			ExecutorService es = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+	
+			// creating the callables
+	//		final Callable<Object> ahCallable = Executors.callable(new CrashOnAssertionErrorRunnable(ah));
+			final ImmutableList<Callable<Object>> callables = ImmutableList.of(
+					Executors.callable(ps),
+					Executors.callable(is)
+					);
+			final ImmutableSet.Builder<Callable<Object>> userCallablesBuilder = ImmutableSet.builder();
+			for (SimpleUserI user : userRecord.getUsers()) {
+				userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(user)));
+	//			userCallablesBuilder.add(Executors.callable(user));
+			}
+			for (EventListener listener : ah.getEventListeners()) {
+				userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(listener)));
+	//			userCallablesBuilder.add(Executors.callable(listeners));
+			}
+			for (Runnable runnable : ah.getRunnables()) {
+				userCallablesBuilder.add(Executors.callable(new CrashOnAssertionErrorRunnable(runnable)));
+	//			userCallablesBuilder.add(Executors.callable(listeners));
+			}
+			
+			ImmutableSet<Callable<Object>> userCallables = userCallablesBuilder.build();
+			
+			// starting the loops - each loop is 1 time unit
+			// 24 * 60 / 5 == 1 day
+			int timeUnits = AuctionLength.ONE_DAY.length() * 100;
+	//		long tenPercent = (long) (timeUnits/10);
+			for (int i = 0; i < timeUnits; i++) {
+	//			if ((i + 1) % tenPercent == 0) {
+	//				logger.warn("another 10% done");
+	//			}
+	//			System.out.println("time unit: " + i);
+				try {
+					ah.run();
+					bh.startUserTurn();
+					
+					// invokeAll() blocks until all tasks are complete
+					es.invokeAll(callables); 
+					es.invokeAll(userCallables);
+					
+					bh.startAhTurn();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+	
+			// clean up
+			ah.saveUsers();
+			es.shutdown();
+			saveObjects.cleanup();
+	
+			logger.debug("Simulation done.");
+		}
+
+}
